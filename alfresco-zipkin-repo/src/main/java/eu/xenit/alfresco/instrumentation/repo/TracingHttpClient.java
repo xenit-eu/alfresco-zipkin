c785f016d72c7039fbff286cc7755437274651ae
Dries De Proost <dries.deproost@xenit.eu>
2021-03-24 16:16:55 +0100

    tmp shared peer progr. commit, playing with Alfresco 6.0 setup

--- alfresco-zipkin-repo/src/main/java/eu/xenit/alfresco/instrumentation/repo/TracingHttpClient.java
+++ alfresco-zipkin-repo/src/main/java/eu/xenit/alfresco/instrumentation/repo/TracingHttpClient.java
@@ -8,6 +8,8 @@ import brave.propagation.TraceContext;
 import org.apache.commons.httpclient.*;
 
 import java.io.IOException;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.util.EncodingUtil;
 
 /**
  * Http Client with tracing functionality
@@ -35,6 +37,12 @@ public class TracingHttpClient extends HttpClient {
     public int executeMethod(HostConfiguration hostconfig, HttpMethod method, HttpState state) throws IOException {
         int responseStatus = 0;
         Span span = httpClientHandler.handleSend(injector, method);
+        span.tag("custom tag key","custom tag value");
+        if (method instanceof PostMethod) {
+            PostMethod postMethod = (PostMethod) method;
+            String s = EncodingUtil.formUrlEncode(postMethod.getParameters(), postMethod.getRequestCharSet());
+            span.tag("possibly a big content string", s);
+        }
         Throwable error = null;
         try (Tracer.SpanInScope ws = tracer.withSpanInScope(span)) {
             responseStatus = super.executeMethod(hostconfig, method, state);
