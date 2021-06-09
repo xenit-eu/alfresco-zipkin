48077240780b994ae7c0fd20265f37e7839e6589
Dries De Proost <dries.deproost@xenit.eu>
2021-06-09 17:09:05 +0200

    tmp

--- alfresco-zipkin-repo/src/main/java/eu/xenit/alfresco/instrumentation/repo/TracingHttpClient.java
+++ alfresco-zipkin-repo/src/main/java/eu/xenit/alfresco/instrumentation/repo/TracingHttpClient.java
@@ -5,19 +5,31 @@ import brave.Tracer;
 import brave.http.HttpClientHandler;
 import brave.http.HttpTracing;
 import brave.propagation.TraceContext;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.apache.commons.httpclient.*;
 
 import java.io.IOException;
+import org.apache.commons.httpclient.params.HttpMethodParams;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Http Client with tracing functionality
  */
 public class TracingHttpClient extends HttpClient {
 
+    private static final Logger logger = LoggerFactory.getLogger(TracingHttpClient.class);
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
     private HttpTracing httpTracing;
     private Tracer tracer;
     private HttpClientHandler<HttpMethod, Integer> httpClientHandler;
     private TraceContext.Injector<HttpMethod> injector;
+
+    private static final Pattern queryPattern = Pattern.compile("spellcheck.q=([^&]*)&");
 
     public TracingHttpClient(HttpTracing httpTracing, HttpConnectionManager httpConnectionManager) {
         super(httpConnectionManager);
@@ -36,12 +48,36 @@ public class TracingHttpClient extends HttpClient {
         int responseStatus = 0;
         Span span = httpClientHandler.handleSend(injector, method);
         Throwable error = null;
+        span.annotate("Solr post Query");
+
         try (Tracer.SpanInScope ws = tracer.withSpanInScope(span)) {
+            HttpMethodParams params = method.getParams();
+            params.setParameter("solrDebug", "on");
+            method.setParams(params);
             responseStatus = super.executeMethod(hostconfig, method, state);
         } catch (RuntimeException | Error e) {
             error = e;
             throw e;
         } finally {
+            String responseBodyAsString = method.getResponseBodyAsString();
+            Matcher queryMatcher = queryPattern.matcher(responseBodyAsString);
+            //  logger.info("number found:" + queryMatcher.());
+            logger.info(String.format("found a match for the query: %s", queryMatcher.matches()));
+            if (queryMatcher.find()) {
+                logger.info("match found through group:" + queryMatcher.group(0));
+                span.tag("query_group", queryMatcher.group(0));
+                String query = responseBodyAsString.subSequence(queryMatcher.start(), queryMatcher.end()).toString();
+                logger.info("query found: " + query);
+                span.tag("query", query);
+            }
+
+            JSONObject jsonResponse = new JSONObject(responseBodyAsString);
+            span.tag("Number of results found",((JSONObject)jsonResponse.get("response")).get("numFound").toString());
+
+            String Qtime = ((JSONObject)jsonResponse.get("response")).get("numFound").toString();
+            span.tag("Qtime", Qtime);
+
+            span.tag("body", method.getResponseBodyAsString());
             httpClientHandler.handleReceive(responseStatus, error, span);
         }
         return responseStatus;
