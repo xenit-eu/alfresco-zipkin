8993a2aa69d9c21bd4cce25890261c2b8744202f
Ranec Belpaire <ranec.belpaire@gmail.com>
2021-09-24 09:08:41 +0200

    XENOPS-831 Refactoring and deletion of files

--- integration-tests/src/test/java/eu/xenit/alfresco/instrumentation/AlfrescoTest.java
+++ integration-tests/src/test/java/eu/xenit/alfresco/instrumentation/AlfrescoTest.java
@@ -1,6 +1,6 @@
 package eu.xenit.alfresco.instrumentation;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
@@ -9,6 +9,7 @@ public class AlfrescoTest {
 
     @Test
     public void alfrescoWebdavUp() {
+        System.out.println("TEST TEST TEST TEST TEST TEST TEST TEST TEST");
         given()
                 .auth().basic(IntegrationTestUtil.ALFRESCO_USERNAME, IntegrationTestUtil.ALFRESCO_PASSWORD)
                 .log().uri()
